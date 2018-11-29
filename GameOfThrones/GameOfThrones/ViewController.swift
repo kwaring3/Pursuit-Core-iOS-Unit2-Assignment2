a5c3e35dc9a6ee646c37bf4a090ff60aa057a8f1
kwaring3 <kevinwaring@pursuit.org>
2018-11-29 16:28:38 -0500

    Added Missing file

---
+++ GameOfThrones/GameOfThrones/ViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  GameOfThrones
+//
+//  Created by Alex Paul on 11/16/18.
+//  Copyright © 2018 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var episodes = GOTEpisode.allEpisodes
+    var array = [[GOTEpisode]]()
+    var gotSeason: Int? = nil
+    var seasonEp = [Int]()
+    
+    @IBOutlet weak var tableView2: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView2.delegate = self
+        tableView2.dataSource = self
+        for episode in episodes {
+            let season = episode.season
+            if season != gotSeason {
+                array.append([])
+                seasonEp.append(season)
+                gotSeason = season
+            }
+            array[array.endIndex - 1].append(episode)
+        }
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let destination = segue.destination as? episodeViewController,
+            let cellSelected = tableView2.indexPathForSelectedRow else {return}
+        let episodeSelected = array[cellSelected.section][cellSelected.row]
+        destination.episodeN = episodeSelected
+    }
+    
+    
+}
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+}
+
+
+extension ViewController: UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return array.count
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cellString: String
+        if array[indexPath.section][indexPath.row].season % 2 == 0 {
+            cellString = "showCellTwo"
+        } else {
+            cellString = "showCell"
+        }
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellString, for: indexPath) as? showCell else {return UITableViewCell()}
+        let episodeToSet = array[indexPath.section][indexPath.row]
+        cell.gotImage.image = UIImage(named: episodeToSet.mediumImageID)
+        cell.gotTitle.text = episodeToSet.name
+        cell.gotInfo.text = "Season: \(episodeToSet.season) Episode: \(episodeToSet.number)"
+        return cell
+    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Season \(seasonEp[section])"
+    }
+}
+
+
+
